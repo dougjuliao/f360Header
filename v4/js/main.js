cbcb2f9606742693ab8b5602423b56d804f1511a
Douglas Julião Marques <douglas_juliao16@hotmail.com>
2020-05-26 15:56:23 -0300

    v4 - mobile e add funcoes js

---
+++ v4/js/main.js
@@ -0,0 +1,22 @@
+const menuCadastroBtn = document.querySelector('.dropdownCadastro');
+const menuBtn = document.querySelector('#menuMobileF360');
+const menuSideBar = document.querySelector('#sidenavMenu');
+
+menuBtn.addEventListener('click', (el) => { // Menu sidebar mobile
+    menuSideBar.classList.toggle('sidenav-mobile');
+});
+menuCadastroBtn.addEventListener('click', (el) => { // Menu de cadastro
+    document.querySelector('.menu-cadastro .dropdown-box').classList.toggle('show');
+});
+window.addEventListener('click', (event) => {
+    if (!event.target.matches('.dropdown-btn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-box");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+});
