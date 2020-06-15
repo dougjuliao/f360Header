ec1bd7e68b43c6da74ac67a8b719dded0de08faf
Douglas Julião Marques <douglas_juliao16@hotmail.com>
2020-06-15 16:57:51 -0300

    Add - js para menu footer mobile

--- v6/js/main.js
+++ v6/js/main.js
@@ -2,6 +2,7 @@ const menuCadastroBtn = document.querySelector('.dropdownCadastro');
 const menuUsuarioBtn = document.querySelector('.menu-user .dropdownUser');
 const menuBtn = document.querySelector('#menuMobileF360');
 const menuSideBar = document.querySelector('#sidenavMenu');
+const menuFooterMobile = document.querySelector('.bottom-navigation-mobile');
 
 menuBtn.addEventListener('click', (el) => { // Menu sidebar mobile
     menuSideBar.classList.toggle('sidenav-mobile');
@@ -10,8 +11,10 @@ menuCadastroBtn.addEventListener('click', (el) => { // Menu de cadastro
     document.querySelector('.menu-cadastro .dropdown-box').classList.toggle('show');
 });
 menuUsuarioBtn.addEventListener('click', (el) => { // Opts Usuário
-    console.log(el)
     document.querySelector('.menu-user .dropdown-box').classList.toggle('show');
+});
+menuFooterMobile.addEventListener('click', (el) => { // Opts Usuário
+    document.querySelector('.footer-navigation').classList.toggle('active');
 });
 window.addEventListener('click', (event) => { // evento de clicar fora do dropdown
     if (!event.target.matches('.dropdown-btn')) {
